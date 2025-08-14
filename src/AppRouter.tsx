e1cc13792756016092aee4dcd25736f31b96fe77
Tristan <djnewage1220@gmail.com>
2025-08-13 19:18:20 -0500

    added slash

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { App } from "./App";
 export function AppRouter() {
   return (
-    <BrowserRouter basename="/shoot-aesthetics">
+    <BrowserRouter basename="/shoot-aesthetics/">
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
